2485fb4d8cd5146847fe97aa4e72d1c8f27eb323
Lekha S <lekha.sikakollu@gmail.com>
2022-06-13 22:24:13 -0700

    Initial project setup and queries

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const colors = require('colors');
+require('dotenv').config();
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema');
+const connectDB = require('./config/db');
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+//Connection to DB
+connectDB();
+
+//Mount a GraphQL server on '/graphql' endpoint
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === 'development',
+}));
+
+app.listen(port, console.log(`Server running on port ${port}`));
